1b16d886929da171207df9eca7d534d89f60b069
Donovan <donniemassenburg75@gmail.com>
2022-06-22 21:35:15 -0400

    added new potion and player constructor

---
+++ _tests_/player.test.js
@@ -0,0 +1,15 @@
+const Player = require('../Player.js')
+const Potion = require('../Potion.js');
+
+jest.mock('../Potion.js');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)])
+      );
+  });
